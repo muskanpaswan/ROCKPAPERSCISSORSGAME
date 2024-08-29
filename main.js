1e5ab0ab7a0bae452f633765e7bdbaa77ba480d7
muskan paswan <muskanpaswan113@gmail.com>
2024-08-29 10:37:12 +0530

    first commit

--- main.js
+++ main.js
@@ -1,54 +1,59 @@
-const paper = document.querySelector("#paper");
-const rock = document.querySelector("#rock");
-const scissor = document.querySelector("#scissor");
-const computerImage = document.querySelector(".computer");
-const userImage = document.querySelector(".user");
-const result = document.querySelector("#result");
+let paper = document.querySelector("#paper");
+let rock = document.querySelector("#rock");
+let scissor = document.querySelector("#scissor");
+let computerImage = document.querySelector(".computer");
+let userImage = document.querySelector(".user");
+let won = document.querySelector("h2");
+
+
+
+
 paper.addEventListener("click", () => {
-    const userchoice = "paper";
+    const userchoice = "paper"
     const computerchoice = randomchoice();
-    const winningMessage = compare(userchoice, computerchoice);
-    userImage.src = `${userchoice}.png`;
-    computerImage = `${computerchoice}.png`;
-    result.textContent = `${winningMessage};`
+    const winningMessage = compare(computerchoice , userchoice);
+    computerImage.src = `./images/${computerchoice}.png`;
+    userImage.src = `./images/${userchoice}.png`;
+    won.textContent = winningMessage;
 })
 rock.addEventListener("click", () => {
     const userchoice = "rock";
     const computerchoice = randomchoice();
-    const winningMessage = compare(userchoice, computerchoice);
-    userImage.src = `${userchoice}.png`;
-    computerImage = `${computerchoice}.png`;
-    result.textContent = winningMessage;
+    const winningMessage = compare(computerchoice , userchoice);
+    computerImage.src = `./images/${computerchoice}.png`
+    userImage.src = `./images/${userchoice}.png`
+    won.textContent = winningMessage;
 })
 scissor.addEventListener("click", () => {
-    const userchoice = "scissor";
+    const userchoice = "scissor"
     const computerchoice = randomchoice();
-    const winningMessage = compare(userchoice, computerchoice);
-    userImage.src = `${userchoice}.png`;
-    computerImage = `${computerchoice}.png`;
-    result.textContent = winningMessage;
+    const winningMessage = compare(computerchoice , userchoice)
+    computerImage.src = `./images/${computerchoice}.png`;
+    userImage.src = `./images/${userchoice}.png`;
+    won.textContent = winningMessage;
 })
 
 function randomchoice() {
     let choice = ["rock", "paper", "scissor"];
-    let randomchoice = choice[Math.floor(Math.random() * choice.length)];
-    return choice[randomchoice];
+    let randomchoice = Math.floor(Math.random() * choice.length)
+    return choice[randomchoice]
 }
 
-function compare(userchoice, computerchoice) {
+function compare(computerchoice, userchoice) {
     if (userchoice === computerchoice) {
-        return "tie"
+        return "TIE..!"
     }
     else if (userchoice === "rock" && computerchoice === "scissor"
         || userchoice === "scissor" && computerchoice === "paper"
         || userchoice === "paper" && computerchoice === "rock"
     ) {
-        return "YOU WON..!" + " " + "your choise" + " " + userchoice + " &" + " " + "computer choise" + " " + computerchoice
+        return "YOU WON..!"
+
     } else if (userchoice === "papaer" && computerchoice === "scissor"
         || userchoice === "scissor" && computerchoice === "rock"
         || userchoice === "rock" && computerchoice === "paper"
     ) {
-        return "COMPUTER WON..!" + " " + "you choise" + " " + userchoice + "&" + " " + " " + computerchoice
+        return "COMPUTER WON..!"
 
     }
 }
